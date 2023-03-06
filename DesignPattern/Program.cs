af3e7b425837f953301396be406d5c738bfea180
KH <KH@DESKTOP-KE662S4>
2023-03-06 17:29:42 +0200

    Add project files.

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //// ============================ for parrallel methods  ============================
+            Parallel.Invoke(
+                () => method1(),
+                () => method2()
+                );
+
+            //// for singelton class
+            //SingletonClass sn = SingletonClass.getInstance();
+            //sn.print("ss");
+
+            //SingletonClass ww = SingletonClass.getInstance();
+            //ww.print("ww");
+
+
+            Console.ReadKey();
+        }
+
+
+        //// ============================ for parrallel methods  ============================
+
+        public static void method1()
+        {
+            SingletonClass s = SingletonClass.getInstance();
+            s.print("hello world");
+        }
+
+        public static void method2()
+        {
+            SingletonClass w = SingletonClass.getInstance();
+            w.print("hello world 222");
+        }
+    }
+}
---
+++ DesignPattern/SingletonClass.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    sealed class SingletonClass
+    {
+        private static int counter;
+        ////============================ used for lazy loading  ============================
+        //// wait unit when call object
+        // private static SingletonClass instance = null;
+
+
+        ////============================ used for eager loading  ============================
+        //// when system start it direct work
+        //private static readonly SingletonClass instance = new SingletonClass();
+
+        ////============================ used for lazy loading  ============================
+        private static readonly Lazy<SingletonClass> instance = new Lazy<SingletonClass>(()=> new SingletonClass());
+
+
+        ////============================ used for threading  ============================
+        //private static readonly object obj = new object();
+
+        //// in Singleton => constructor must be private
+        private SingletonClass()
+        {
+            counter += 1;
+            Console.WriteLine("Istance number " + counter.ToString());
+        }
+
+        //// ============================ used for eager loading ============================
+
+        public static SingletonClass getInstance()
+        {
+            return instance.Value;
+        }
+
+
+        //// ============================ used for lazy loading ============================
+        //public static SingletonClass getInstance()
+        //{
+        //    if (instance == null)
+        //    {
+        //        instance = new SingletonClass();
+        //    }
+
+        //    return instance;
+        //}
+
+        //// ============================ used for threading ============================
+        //public static SingletonClass getInstance()
+        //{
+        //    if (instance == null)
+        //        lock (obj)
+        //        {
+        //            if (instance == null)
+        //            {
+        //                instance = new SingletonClass();
+        //            }
+        //        }
+
+        //    return instance;
+        //}
+
+
+        public void print(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+        //public class son : SingletonClass
+        //{
+
+        //}
+    }
+}
+
+
+
+
+
+// in eager lazy loading all data loaded at first time => big size => lower performance
+// in lazy loading data that i need to use only        => small size => higher performance
